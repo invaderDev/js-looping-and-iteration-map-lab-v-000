66b926cf728cd116e17a3acdf6c91d93866bded8
Kris Thompson <kt.devcat@gmail.com>
2018-01-05 16:51:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,5 +13,7 @@ function nameToAttributes(drivers) {
 }
 
 function attributesToPhrase(drivers) {
-  
+  return drivers.map(function (driver) {
+    return `${driver.name} is from ${driver.hometown}.`;
+  })
 }
