faa3fb89c8a3fe3dbdfc759b4c3fa26f2f27aa23
Kris Thompson <kt.devcat@gmail.com>
2018-01-05 16:50:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ function lowerCaseDrivers(drivers) {
 function nameToAttributes(drivers) {
   return drivers.map(function (driver) {
     const nameArray = driver.split(" ");
-
     const driverObject = Object.assign({}, {firstName : nameArray[0]}, {lastName : nameArray[1]});
 
     return driverObject;
   })
 }
+
